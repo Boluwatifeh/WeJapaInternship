9dac353151011427f06f7722f83869688990b654
boluwatifeh <tifeasypeasy@gmail.com>
2020-07-28 20:35:55 +0100

    solved this lab

---
+++ Lab2of1.py
@@ -0,0 +1,7 @@
+# Write two lines of code below, each assigning a value to a variable
+first_name = "Boluwatife"
+last_name = "Ogundeyi"
+
+# Now write a print statement using .format() to print out a sentence and the
+#   values of both of the variables
+print("My name is {} {}".format(last_name, first_name))
