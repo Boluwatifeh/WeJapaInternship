13c8967501c4c251ffbff15c3b180cf78d61ff20
boluwatifeh <tifeasypeasy@gmail.com>
2020-07-31 16:18:43 +0100

    solved this lab

---
+++ Lab2of2.py
@@ -0,0 +1,23 @@
+#1. What is the length of the string variable verse?
+#2. What is the index of the first occurrence of the word 'and' in verse?
+#3. What is the index of the last occurrence of the word 'you' in verse?
+#4. What is the count of occurrences of the word 'you' in the verse?
+
+
+verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
+print(verse)
+
+# Use the appropriate functions and methods to answer the questions above
+# Bonus: practice using .format() to output your answers in descriptive messages!
+#1. length of variable verse
+length_of_verse = len(verse)
+print("Length of the verse variable is: {}".format(length_of_verse))
+
+#2. What is the index of the first occurrence of the word 'and' in verse?
+print("The index of the first occurence of the word 'and' in verse is: {}".format(verse.index('and')))
+
+#3. What is the index of the last occurrence of the word 'you' in verse?
+print("The index of the last occurence of the word 'and' in verse is: {}".format(verse.rindex('you')))
+
+#4. What is the count of occurrences of the word 'you' in the verse?
+print("The count of the occurrences of the word 'you' in the verse is: {}".format(verse.count('you')))
