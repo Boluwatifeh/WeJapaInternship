9457d7de72aa48e09dfa93011c6c02d2a08fc8ef
boluwatifeh <tifeasypeasy@gmail.com>
2020-08-09 00:07:25 +0100

    added update

--- Wave Three Exercises/dictionary_stuff1.py
+++ Wave Three Exercises/dictionary_stuff1.py
@@ -10,9 +10,6 @@ fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
 #Iterate through the dictionary
 for item, count in basket_items.items():
     if item in fruits:
-        result += count
-
-#if the key is in the list of fruits, add the value (number of fruits) to result
-
+        result += count #if the key is in the list of fruits, add the value (number of fruits) to result
 
 print('Number of fruits in the basket is: {}'.format(result))
