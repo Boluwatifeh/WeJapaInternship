46c09b241989937dceeb0a4954eaba1a87c874a9
boluwatifeh <tifeasypeasy@gmail.com>
2020-08-16 03:03:21 +0100

    solved

---
+++ Wave Four Exercises/Scripting/expquiz.py
@@ -0,0 +1,6 @@
+#It's your turn to import and use the math module. Use the math module to calculate e to the power of 3. print the answer.
+# print e to the power of 3 using the math module
+import math
+
+power_of_three = math.exp(3)
+print("Answe is: {}".format(power_of_three))
